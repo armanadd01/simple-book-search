19d240c0ae79c9d210ca348fb4ff770fdccce33d
Clipping14 <marketingclippingworld14@gmail.com>
2021-09-02 17:44:00 +0600

    changes

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const searchBtn = document.getElementById("search-btn");
 const bookContainer = document.getElementById("book-container");
 const errorDiv = document.getElementById("error");
 const spinner = document.getElementById("spinner");
-const authorName = document.getElementById("author-name");
+const filterData = document.getElementById("author-name");
 
 
 
@@ -41,32 +41,29 @@ const showSearchResult = docs => {
         errorDiv.innerText = "";
     }
 
+    // 
     docs.forEach(book => {
-        // console.log(book.author_name);
         //Author Check 
-        const validCheck = validname => {
-            if (validname !== undefined) {
-                return validname;
-
+        const isValidated = validation => {
+            if (validation !== undefined) {
+                return validation;
             } else {
                 return "Not Found";
             }
 
         }
-
         const div = document.createElement("div");
         div.classList.add("col-md-3");
         div.innerHTML = `
-      <div class="card mt-3" style="width: 18rem;">
-        <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="card-img-top img-fluid" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${validCheck(book.title)}</h5>
-          <h6 class="card-title">Author ${validCheck(book.author_name)}</h6>
-
-          <p class="card-text">Published ${validCheck(book.first_publish_year)}</p>
-          
-        </div>
-      </div>
+            <div class="card mt-3" style="width: 18rem;">
+                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="card-img-top img-fluid" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">${isValidated(book.title)}</h5>
+                <h6 class="card-title">Author Name: ${isValidated(book.author_name)}</h6>
+                <p class="card-text">Published: ${isValidated(book.first_publish_year)}</p>
+                <p class="card-text">Publisher Name: ${isValidated(book.publisher[0])}</p>
+                </div>
+            </div>
       `;
         bookContainer.appendChild(div);
     });
