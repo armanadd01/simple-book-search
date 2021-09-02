79b1ff981956f37851e0483aff662823b24c330f
Clipping14 <marketingclippingworld14@gmail.com>
2021-09-02 12:55:05 +0600

    another commit

--- js/index.js
+++ js/index.js
@@ -1,64 +1,73 @@
 const searchInput = document.getElementById("searchInput");
 const searchBtn = document.getElementById("search-btn");
 const bookContainer = document.getElementById("book-container");
-const bookDetails = document.getElementById("book-details");
 const errorDiv = document.getElementById("error");
 const spinner = document.getElementById("spinner");
+const authorName = document.getElementById("author-name");
 
 
 
-searchBtn.addEventListener("click", function () {
-  const searchText = searchInput.value;
-  if (searchText === "") {
-    errorDiv.innerText = "Search field cannot be empty.";
-    return;
-  }
-  //   Clear
-  bookContainer.innerHTML = "";
-  bookDetails.innerHTML = "";
-  const url = `http://openlibrary.org/search.json?q=${searchText}`;
-  console.log(url);
-  spinner.classList.remove("d-none");
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      // spinnner and showing data
-      setTimeout(() => {
-        spinner.classList.add("d-none");
-        showSearchResult(data.docs);
-      }, 1000);
-    })
-    .finally(() => {
-      searchInput.value = "";
-    });
+searchBtn.addEventListener("click", function() {
+    const searchText = searchInput.value;
+    if (searchText === "") {
+        errorDiv.innerText = "Search field cannot be empty.";
+        return;
+    }
+    //   Clear
+    bookContainer.innerHTML = "";
+    // bookDetails.innerHTML = "";
+    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+    spinner.classList.remove("d-none");
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            // spinnner and showing data
+            setTimeout(() => {
+                spinner.classList.add("d-none");
+                showSearchResult(data.docs);
+            }, 1000);
+        })
+        .finally(() => {
+            searchInput.value = "";
+        });
 });
 
 const showSearchResult = docs => {
-    // console.log(docs);
-  // Error Handing
-  if (docs.length === 0 ) {
-    errorDiv.innerText = "No Search Result Found";
-  } else {
-    errorDiv.innerText = "";
-  }
 
-  docs.forEach(book => {
-    // console.log(book);
-    const div = document.createElement("div");
-    div.classList.add("col-md-3");
-    div.innerHTML = `
-      <div class="card" style="width: 18rem;">
-        <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="card-img-top" alt="...">
+    // Error Handing
+    if (docs.length === 0) {
+        errorDiv.innerText = "No Search Result Found";
+    } else {
+        errorDiv.innerText = "";
+    }
+
+    docs.forEach(book => {
+        // console.log(book.author_name);
+        //Author Check 
+        const validCheck = validname => {
+            if (validname !== undefined) {
+                return validname;
+
+            } else {
+                return "Not Found";
+            }
+
+        }
+
+        const div = document.createElement("div");
+        div.classList.add("col-md-3");
+        div.innerHTML = `
+      <div class="card mt-3" style="width: 18rem;">
+        <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="card-img-top img-fluid" alt="...">
         <div class="card-body">
-          <h5 class="card-title">${book.title}</h5>
-          <h6 class="card-title">${book.author_name}</h6>
+          <h5 class="card-title">${validCheck(book.title)}</h5>
+          <h6 class="card-title">Author ${validCheck(book.author_name)}</h6>
 
-          <p class="card-text">Published ${book.first_publish_year}</p>
+          <p class="card-text">Published ${validCheck(book.first_publish_year)}</p>
           
         </div>
       </div>
       `;
-    bookContainer.appendChild(div);
-  });
+        bookContainer.appendChild(div);
+    });
 }
-
